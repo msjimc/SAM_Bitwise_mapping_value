16c1c44a0e14a4e0bc50f1f3efab2c7bc3c9780e
Dr Ian M. Carr <msjimc@leeds.ac.uk>
2024-10-03 13:56:44 +0100

    Add project files.

---
+++ SAM_Bitwise_mapping_value/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SAM_Bitwise_mapping_value
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFlag = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtFlag);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(527, 99);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bit value";
+            // 
+            // txtFlag
+            // 
+            this.txtFlag.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFlag.Location = new System.Drawing.Point(9, 66);
+            this.txtFlag.Name = "txtFlag";
+            this.txtFlag.Size = new System.Drawing.Size(512, 20);
+            this.txtFlag.TabIndex = 1;
+            this.txtFlag.TextChanged += new System.EventHandler(this.txtFlag_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thr nature of an alignment is described in a SAM file\'s flag value which is in co" +
+    "lumn 2 of an alignment. If you copy this value to the text box below, the alignm" +
+    "ents properties will be shown below. ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtResult);
+            this.groupBox2.Location = new System.Drawing.Point(12, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(527, 192);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Alignments properties";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(9, 19);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(512, 167);
+            this.txtResult.TabIndex = 0;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnQuit.Location = new System.Drawing.Point(458, 315);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 350);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(567, 389);
+            this.Name = "Form1";
+            this.Text = "Bitwise flag value";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFlag;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
+
---
+++ SAM_Bitwise_mapping_value/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace SAM_Bitwise_mapping_value
+{
+    public partial class Form1 : Form
+    {
+        readonly string[] _valuesPositive = { "Paired data", "Both reads mapped as a pair", "Read unmapped", "Mate is unmapped", "Read reverse complemented", "Mate read is reverse complemented", "Read mapped to template", "Read's mate is mapped to template", "This ss a secondary alignment", "Alignment fails quality checks", "PCR or optical duplicate", "Is in supplementary alignment (e.g. aligner specific, could be a portion of a split read or a tied region)" };
+        readonly string[] _valuesNegative = { "Single end data", "Not a mapped read pair", "Read mapped", "Mate is mapped", "Read in original orientation", "Mate pair isnt reverse complement", "Read mapped to template", "Read's mate not mapped to template", "This is a secondary alignment", "Alignment fails quality checks", "PCR or optical duplicate", "Is in supplementary alignment (e.g. aligner specific, could be a portion of a split read or a tied region)" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtFlag_TextChanged(object sender, EventArgs e)
+        {
+            string[] valuesPositive = _valuesPositive;
+            string[] valuesNegative = _valuesNegative;
+
+            txtResult.Clear();
+            int value = -1;
+            try
+            {
+              value = Convert.ToInt32(txtFlag.Text.Trim());
+                string answer = "";
+               if ((value & 1) == 1)
+                { answer += valuesPositive[0] + "\r\n"; }
+                else
+                {
+                    answer += valuesNegative[0] + "\r\n";
+                    int[] delete = { 1, 5, 7 };
+                    valuesNegative=clean(valuesNegative, delete);
+                    valuesPositive = clean(valuesPositive, delete);
+                }
+
+                if ((value & 2) == 2)
+                { answer += valuesPositive[1] + "\r\n"; }
+                else
+                { answer += valuesNegative[1] + "\r\n"; }
+
+                if ((value & 4) == 4)
+                {
+                    answer += valuesPositive[2] + "\r\n";   
+                    int[] delete = { 4, 6, 8,10 ,11};
+                    valuesNegative = clean(valuesNegative, delete);
+                    valuesPositive = clean(valuesPositive, delete);
+                }
+                else 
+                {  answer += valuesNegative[2] + "\r\n"; }
+
+                if ((value & 8) == 8)
+                { 
+                    answer += valuesPositive[3] + "\r\n"; 
+                    int[] delete = { 5, 7, 8,10,11};
+                    valuesNegative = clean(valuesNegative, delete);
+                    valuesPositive = clean(valuesPositive, delete);
+                }
+                else 
+                {  answer += valuesNegative[3] + "\r\n"; }
+
+                if ((value & 16) == 16)
+                { answer += valuesPositive[4] + "\r\n"; }
+                else { answer += valuesNegative[4] + "\r\n"; }
+
+                if ((value & 32) == 32)
+                { answer += valuesPositive[5] + "\r\n"; }
+                else { answer += valuesNegative[5] + "\r\n"; }
+
+                if ((value & 64) == 64)
+                { answer += valuesPositive[6] + "\r\n"; }
+                else { answer += valuesNegative[6] + "\r\n"; }
+
+                if ((value & 128) == 128)
+                { answer += valuesPositive[7] + "\r\n"; }
+                else { answer += valuesNegative[7] + "\r\n"; }
+
+                if ((value & 256) == 256)
+                { answer += valuesPositive[8] + "\r\n"; }
+                else { answer += valuesNegative[8] + "\r\n"; }
+
+                if ((value & 512) == 512)
+                { answer += valuesPositive[9] + "\r\n"; }
+                else { answer += valuesNegative[9] + "\r\n"; }
+
+                if ((value & 1024) == 1024)
+                { answer += valuesPositive[10] + "\r\n"; }
+                else { answer += valuesNegative[10] + "\r\n"; }
+
+                if ((value & 2048) == 2048)
+                { answer += valuesPositive[11] + "\r\n"; }
+                else { answer += valuesNegative[11] + "\r\n"; }
+
+                txtResult.Text = answer;
+
+            }
+            catch { txtResult.Clear(); }
+        }
+
+        private string[] clean(string[] array, int[] delete)
+        {
+            List<string> result = new List<string>();
+            result.AddRange(array);
+            foreach (int i in delete)
+            {
+                result[i] = "NA";
+            }
+            return result.ToArray();
+        }
+    }
+}
